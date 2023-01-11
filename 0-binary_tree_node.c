1eecd7f3390b629f7428e0eef979963f92c10c67
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-11 10:07:45 -0800

    tsk 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+*binary_tree_node - create a binatry tree node
+*@parent: pointer to th parent node to create
+*@value: value to the new node
+*
+*Return: new node
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
