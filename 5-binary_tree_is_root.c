04f366908b491e9f69ed4f6edb11ab1d88ba79c8
amorinkaren2021@gmail.com <amorinkaren2021@gmail.com>
2023-01-13 07:13:19 -0800

    fix

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -12,9 +12,9 @@ int binary_tree_is_root(const binary_tree_t *node)
 	{
 		return (0);
 	}
-	if (node->parent)
+	if (!node->parent)
 	{
-		return (1);
+		return (0);
 	}
-	return (0);
+	return (1);
 }
