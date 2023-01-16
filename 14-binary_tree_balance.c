33bf246bb1fa2ebcadf3d871980b5cae2489849d
vviceen <debrunvicente@gmail.com>
2023-01-15 16:28:55 -0800

    removed tries

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,27 +1,17 @@
 #include "binary_trees.h"
 #include "9-binary_tree_height.c"
-
 /**
-*binary_tree_balance - measures the balance factor of a binary tree
+*binary_tree_balance -  measures the balance factor of a binary tree
 *@tree: pointer to the root node of the tree to measure the balance factor
 *
-*Return: if tree is NULL, return 0
+*Return: If tree is NULL, return 0
 */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
-	{
-		if (binary_tree_height(tree->left) == 0 && tree->left)
-		{	
-			return	(1 + binary_tree_height(tree->left) - binary_tree_height(tree->right));
-		}
-		if (binary_tree_height(tree->right) == 0 && tree->right)
-		{
-			return	(binary_tree_height(tree->left) - 1 + binary_tree_height(tree->right));
-		}
 
-		return	(binary_tree_height(tree->left) - binary_tree_height(tree->right));
-	}
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+
 	return (0);
 }
